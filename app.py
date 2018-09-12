44adc4c567bb3844ad21680b4b537c5621efaeed
sol_rosser <solomonj2k12@hotmail.co.uk>
2018-09-12 13:13:09 +0000

    added a form only the forms fileds with icons for the recipe database

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ def get_cusines():
     return render_template("index.html", 
     recipes=mongo.db.recipes.find())
     
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html',
+    cusine=mongo.db.cusine.find(),
+    difficulty=mongo.db.difficulty.find())
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
