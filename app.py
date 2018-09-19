2ea37b0949cd7fa034b75cb1a94e472540f9f9d8
sol_rosser <solomonj2k12@hotmail.co.uk>
2018-09-19 08:37:22 +0000

    added comments to all working function across all files

--- app.py
+++ app.py
@@ -8,50 +8,73 @@ app.config["MONGO_DBNAME"] = 'recipe_database'
 app.config["MONGO_URI"] = 'mongodb://admin:Tottenham18@ds151282.mlab.com:51282/recipe_database'
 
 mongo = PyMongo(app)
-
+'''
+the backend for the username sign up page, the username will be added to users.txt
+then redirect to the page which displays all the recipes
+'''
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request .method == "POST":
         with open("data/users.txt", "a") as user_list:
             user_list.write(request.form["username"] + "/n")
-        return redirect(url_for('loggedin'))
+        return redirect(url_for('get_recipes'))
     return render_template("index.html")
 
+@app.route('/filter ,methods=["POST"]')
+def filter():
+    if request.method == "POST":
+        mongo.db.recipes.find({'cusine_name' : 'Chinese' })
+    return redirect(url_for('get_recipes'))
+'''
+simple app route to render the main page
+'''
 @app.route('/loggedin')
 def loggedin():
-   
-   
-    
     return render_template('loggedin.html')
-    
+'''
+a simple logout feature when the user clicks on logout they will be redirect to the 
+page to chose their username
+'''
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
-    
-@app.route('/get_recipes')
+'''
+this feature fetches all the recipes stored in my mongo database on mlabs 
+'''
+@app.route('/get_recipes, methods=["POST"]')
 def get_recipes():
     return render_template("loggedin.html", 
     recipes=mongo.db.recipes.find())
-    
+'''
+backend code which will help to find cusine and difficulty from the database
+'''
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('add_recipe.html',
     cusine=mongo.db.cusine.find(),
     difficulty=mongo.db.difficulty.find())
-    
+ 
+'''
+ this function send the data entered by the user to the database on mlab
+'''
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
-    
+'''
+it will fetch the id for what recipe the user has decided to edit and add the existing data
+to the fields in the form
+'''
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     the_recipe = mongo.db.recipes.find_one({"_id" : ObjectId(recipe_id)})
     all_cusines = mongo.db.cusine.find()
     all_difficulties = mongo.db.difficulty.find()
     return render_template('edit_recipe.html', index=the_recipe, cusine=all_cusines, difficulty=all_difficulties)
-    
+'''
+this will post the updated changes made by the user and make the changes in mlab
+'''
 @app.route('/update_recipe/<recipe_id>, methods=["POST"]')
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
@@ -66,7 +89,9 @@ def update_recipe(recipe_id):
         'instructions_details':request.form.get['instructions_details']
     })
     return redirect(url_for('get_recipes'))
-    
+'''
+this function simplys deletes the chosen recipe from the database via the id
+'''
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id':ObjectId(recipe_id)})
